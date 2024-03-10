f93b028b47851875e3319e8202cda11508b7bb3b
ACHYUTH <siraachyuth1122@gmail.com>
2024-03-10 18:15:16 +0530

    small change 3 login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	    $username=$_POST['username'];
 	    $password=$_POST['password'];
 
-	    $sql="select * from Login where username='$empid' and password='$emp_password'";
+	    $sql="select * from Login where username='$username' and password='$password'";
 	    $result=mysqli_query($conn,$sql);
 	    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 	    $count=mysqli_num_rows($result);

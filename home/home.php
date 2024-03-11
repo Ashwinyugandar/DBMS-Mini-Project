58675a8bf2a78240030306de281df1dff99d4ab2
ACHYUTH <siraachyuth1122@gmail.com>
2024-03-11 18:20:03 +0530

    new login page

--- home/home.php
+++ home/home.php
@@ -8,15 +8,18 @@
 </head>
 <body>
     <header>
-        <h2>Criminal Database Management</h2>
+
+        <h2>Criminal Database Management</h2> <br>
         <a class="out" href="../index.php">Logout</a>
     </header>
+    <sub>
     <div class="nav">
-        <a class="home" href="home.php">Home</a>
-        <a class="staff" href="">Staff List</a>
-        <a class="active_cases" href="">Active Cases</a>
-        <a class="closed_cases" href="">Closed cases</a>
-        <a class="add_case" href="">Add New Case</a>
+        <a id="nav" class="home" href="home.php">Home</a>
+        <a id="nav" class="staff" href="">Staff List</a>
+        <a id="nav" class="active_cases" href="">Active Cases</a>
+        <a id="nav" class="closed_cases" href="">Closed cases</a>
+        <a id="nav" class="add_case" href="">Add New Case</a>
 </div>
+</sub>
 </body>
 </html>

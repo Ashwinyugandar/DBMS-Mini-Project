11a9631a1631b38edebf4366b9f7ca3b23a32b60
Bhargava K K <bhargavakk13@gmail.com>
2024-03-11 20:17:35 +0530

    Added cases and staff folder

---
+++ staff/staff.php
@@ -0,0 +1,53 @@
+<?php
+include("../connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Page</title>
+    <link rel="stylesheet" href="staff.css">
+</head>
+<body>
+    <div>
+    <header>
+
+        <h2>Criminal Database Management</h2> <br>
+        <a class="out" href="../index.php">Logout</a>
+    </header>
+    <div class="nav">
+        <a id="nav" class="home" href="home.php">Home</a>
+        <a id="nav" class="staff" href="../staff/staff.php">Staff List</a>
+        <a id="nav" class="active_cases" href="">Active Cases</a>
+        <a id="nav" class="closed_cases" href="">Closed cases</a>
+        <a id="nav" class="add_case" href="">Add New Case</a>
+</div>
+</div>
+<div class="php">
+<?php
+$sql = "SELECT * FROM Staff";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>Staff ID</th><th>Staff Name</th><th>Designation</th><th>Date of Joining</th></tr>";
+
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["Staff_id"] . "</td>";
+        echo "<td>" . $row["Staff_name"] . "</td>";
+        echo "<td>" . $row["Designation"] . "</td>";
+        echo "<td>" . $row["DOJ"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
+</div>
+</body>
+</html>

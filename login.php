485e1ee3e720427d63e8a759946b4800ce684236
Bhargava K K <bhargavakk13@gmail.com>
2024-03-11 18:38:46 +0530

    changes in login file

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	    $username=$_POST['username'];
 	    $password=$_POST['password'];
 
-	    $sql="select * from Login where username='$username' and password='$password'";
+	    $sql="select * from Login where Staff_id='$username' and Password='$password'";
 	    $result=mysqli_query($conn,$sql);
 	    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
 	    $count=mysqli_num_rows($result);
